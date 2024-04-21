bd3b9b13fb889b99594bac611e89b024ab931bae
grodier <grodier3@gmail.com>
2024-04-20 23:51:22 -0400

    Update 1.11 to better align with solution

--- main.go
+++ main.go
@@ -5,35 +5,15 @@ import (
 	"net/http"
 )
 
-func middlewareCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	const FILE_ROOT_PATH = "."
 	const PORT = "8080"
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(FILE_ROOT_PATH))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(FILE_ROOT_PATH))))
-
-	corsMux := middlewareCORS(mux)
+	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
 		Addr:    ":" + PORT,
@@ -42,4 +22,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", FILE_ROOT_PATH, PORT)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }

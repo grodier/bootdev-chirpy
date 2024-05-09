23035b71bb65d7d2c870d9db97c684a311655de0
grodier <grodier3@gmail.com>
2024-05-08 22:16:22 -0400

    Add author id query param to filter

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -27,19 +27,33 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
+
 	dbChirps, err := cfg.DB.GetChirps()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
 		return
 	}
 
+	authorId := -1
+	authorIdStr := r.URL.Query().Get("author_id")
+	if authorIdStr != "" {
+		authorId, err = strconv.Atoi(authorIdStr)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID")
+		}
+	}
+
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		if authorId != -1 && authorId != dbChirp.AuthorId {
+			continue
+		}
 		chirps = append(chirps, Chirp{
 			ID:       dbChirp.ID,
 			Body:     dbChirp.Body,
 			AuthorId: dbChirp.AuthorId,
 		})
+
 	}
 
 	sort.Slice(chirps, func(i, j int) bool {

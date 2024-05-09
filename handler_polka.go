2a7e7739091270b0a244ee2268f84efde44f4f22
grodier <grodier3@gmail.com>
2024-05-08 21:57:34 -0400

    Add apikey to webhook

--- handler_polka.go
+++ handler_polka.go
@@ -3,9 +3,29 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func (cfg *apiConfig) handlerPolka(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		respondWithError(w, http.StatusUnauthorized, "Authorization header is required")
+		return
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "ApiKey" {
+		respondWithError(w, http.StatusUnauthorized, "Authorization header must be in the format 'ApikKey {key}'")
+		return
+	}
+
+	apiKey := parts[1]
+	if apiKey != os.Getenv("POLKA_API_KEY") {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	type Data struct {
 		UserId int `json:"user_id"`
 	}
